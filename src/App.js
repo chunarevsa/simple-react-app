1fac7e273394f95fc2c3e6dca4376ef170d8aac5
Sergei Chunarev <sergey.chunarev@ural.rt.ru>
2023-08-30 16:08:52 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+function App() {
+    const [likes, setLikes] = useState(1)
+
+    const [value, setValue] = useState('Some text')
+    function increment() {
+        setLikes(likes + 1)
+    }
+
+    function decrement() {
+        setLikes(likes - 1)
+    }
+
+    return (
+        <div className="App">
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input
+                type='text'
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    );
+}
+
+export default App;
