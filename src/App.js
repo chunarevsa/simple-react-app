d03364bd5157577144feb3258f24cfb4944c6d2c
Sergei Chunarev <sergey.chunarev@ural.rt.ru>
2023-08-30 22:34:41 +0200

    add list of Posts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, {useState} from 'react';
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    const [value, setValue] = useState('Some text')
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Post', body: "Description"},
+        {id: 2, title: 'Post', body: "Description"},
+        {id: 3, title: 'Post', body: "Description"},
+        {id: 4, title: 'Post', body: "Description"},
+    ])
 
     return (
         <div className="App">
-            <Counter/>
-            <ClassCounter/>
+            {/*<Counter/>*/}
+            {/*<ClassCounter/>*/}
+            <PostList posts={posts} title="Список постов 1"/>
         </div>
     );
 }
