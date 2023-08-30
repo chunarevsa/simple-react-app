0099028ed80b45436ccc6ba547ea68163c4f4614
Sergei Chunarev <sergey.chunarev@ural.rt.ru>
2023-08-31 00:00:42 +0200

    add adding posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import './styles/App.css';
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -10,11 +11,45 @@ function App() {
         {id: 3, title: 'Post', body: "Description"},
         {id: 4, title: 'Post', body: "Description"},
     ])
+    const [post, setPost] = useState({title: '', body: ''})
+
+    // const bodyInputRef = useRef()
+
+    function addNewPost(e) {
+        e.preventDefault()
+        setPosts([...posts, {...post, id: Date.now()}])
+        setPost({title: '', body: ''})
+    }
 
     return (
         <div className="App">
-            {/*<Counter/>*/}
-            {/*<ClassCounter/>*/}
+            <form>
+                {/*Управляемый (контролируемый) компонент*/}
+                <MyInput
+                    value={post.title}
+                    onChange={a => setPost({
+                        ...post, title: a.target.value
+                    })}
+                    type="text"
+                    placeholder="Название поста"/>
+
+                {/*НЕ управляемый (НЕ контролируемый) компонент*/}
+                {/*<MyInput*/}
+                {/*    ref={bodyInputRef}*/}
+                {/*    type="text"*/}
+                {/*    placeholder="Описание"/>*/}
+                {/*<MyButton onClick={addNewPost}> Опубликовать пост </MyButton>*/}
+
+                <MyInput
+                    value={post.body}
+                    onChange={a => setPost({
+                        ...post, body: a.target.value
+                    })}
+                    type="text"
+                    placeholder="Описание"/>
+
+                <MyButton onClick={addNewPost}> Опубликовать пост </MyButton>
+            </form>
             <PostList posts={posts} title="Список постов 1"/>
         </div>
     );
