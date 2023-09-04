f09c28d9d0e51a5131895299ad47c73a900dbdda
Sergei Chunarev <sergey.chunarev@ural.rt.ru>
2023-09-04 16:12:23 +0200

    add loader to all requests

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./api/PostService";
 import Loader from "./components/UI/loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [isPostsLoading, setIsPostsLoading] = useState(false)
-
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll()
+        setPosts(posts)
+    })
+    
     useEffect(() => {
         fetchPosts()
     }, [])
@@ -27,13 +31,6 @@ function App() {
 
     function removePost(post) {
         setPosts(posts.filter(p => p.id !== post.id))
-    }
-
-    async function fetchPosts() {
-        setIsPostsLoading(true)
-        const posts = await PostService.getAll()
-        setPosts(posts)
-        setIsPostsLoading(false)
     }
 
     return (
@@ -52,6 +49,9 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
+            }
             <hr style={{margin: '15px 0'}}/>
             {isPostsLoading
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
--- src/api/PostService.js
+++ src/api/PostService.js
@@ -3,13 +3,7 @@ import axios from "axios";
 export default class PostService {
 
     static async getAll() {
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-            return response.data
-        } catch (e) {
-            console.log(e)
-            throw e
-        }
-
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        return response.data
     }
 }
